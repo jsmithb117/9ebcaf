f39057c3ae49a3d3867ed4098fca43dc14772760
jsmithb117 <jsmithb117@gmail.com>
2021-12-09 12:15:08 -0800

    Removes immer and reverts to initial commit

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,4 +1,3 @@
-import produce from 'immer';
 export const addMessageToStore = (state, payload) => {
   const { message, sender } = payload;
   // if sender isn't null, that means the message needs to be put in a brand new convo
@@ -9,72 +8,73 @@ export const addMessageToStore = (state, payload) => {
       messages: [message],
     };
     newConvo.latestMessageText = message.text;
-    return produce(state, (draft) => {
-      draft.unshift(newConvo);
-      return draft;
-    });
+    return [newConvo, ...state];
   }
-  return produce(state, (draft) => {
-    draft.forEach((convo, index) => {
-      if (convo.id === message.conversationId) {
-        draft[index].messages.unshift(message);
-        draft[index].latestMessageText = message.text;
-      }
+
+  return state.map((convo) => {
+    if (convo.id === message.conversationId) {
+      convo.messages.push(message);
+      convo.latestMessageText = message.text;
       return convo;
-    });
-    return draft;
+    } else {
+      return convo;
+    }
   });
 };
 
 export const addOnlineUserToStore = (state, id) => {
-  return produce(state, (draft) => {
-    draft.forEach((convo) => {
-      if (convo.otherUser.id === id) {
-        convo.otherUser.online = true;
-      }
+  return state.map((convo) => {
+    if (convo.otherUser.id === id) {
+      const convoCopy = { ...convo };
+      convoCopy.otherUser.online = true;
+      return convoCopy;
+    } else {
       return convo;
-    });
-    return draft;
+    }
   });
 };
 
 export const removeOfflineUserFromStore = (state, id) => {
-  return produce(state, (draft) => {
-    draft.forEach((convo) => {
-      if (convo.otherUser.id === id) {
-        convo.otherUser.online = false;
-      }
+  return state.map((convo) => {
+    if (convo.otherUser.id === id) {
+      const convoCopy = { ...convo };
+      convoCopy.otherUser.online = false;
+      return convoCopy;
+    } else {
       return convo;
-    });
-    return draft;
+    }
   });
 };
 
 export const addSearchedUsersToStore = (state, users) => {
   const currentUsers = {};
-  return produce(state, (draft) => {
-    draft.forEach((convo) => {
-      currentUsers[convo.otherUser.id] = true;
-    });
-    users.forEach((user) => {
-      if (!currentUsers[user.id]) {
-        let fakeConvo = { otherUser: user, messages: [] };
-        draft.push(fakeConvo);
-      }
-    });
-    return draft;
-  })
+
+  // make table of current users so we can lookup faster
+  state.forEach((convo) => {
+    currentUsers[convo.otherUser.id] = true;
+  });
+
+  const newState = [...state];
+  users.forEach((user) => {
+    // only create a fake convo if we don't already have a convo with this user
+    if (!currentUsers[user.id]) {
+      let fakeConvo = { otherUser: user, messages: [] };
+      newState.push(fakeConvo);
+    }
+  });
+
+  return newState;
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-  return produce(state, (draft) => {
-    draft.forEach((convo) => {
-      if (convo.otherUser.id === recipientId) {
-        convo.id = message.conversationId;
-        convo.messages.unshift(message);
-        convo.latestMessageText = message.text;
-      }
-      return convo
-    });
+  return state.map((convo) => {
+    if (convo.otherUser.id === recipientId) {
+      convo.id = message.conversationId;
+      convo.messages.push(message);
+      convo.latestMessageText = message.text;
+      return convo;
+    } else {
+      return convo;
+    }
   });
 };
