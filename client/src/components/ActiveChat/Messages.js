0abe90463154739ccf9fe06269c220ca773733d4
jsmithb117 <jsmithb117@gmail.com>
2021-12-12 14:59:28 -0800

    Recieves notification related props and passes them to OtherUserBubble

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -4,7 +4,7 @@ import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId, conversationId } = props;
 
   return (
     <Box>
@@ -14,7 +14,7 @@ const Messages = (props) => {
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} messageId={message.id} messageIsRead={message.read} conversationId={conversationId} />
         );
       })}
     </Box>
