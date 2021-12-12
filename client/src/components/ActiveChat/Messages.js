a07f2dc7a0e5874664b9ae10dd3b105cd87dbe94
jsmithb117 <jsmithb117@gmail.com>
2021-12-12 15:59:19 -0800

    Passes isLatestReadMessage boolean to SenderBubble for conditional rendering

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -4,15 +4,17 @@ import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId, conversationId } = props;
+  const { messages, otherUser, userId, conversationId, latestMessageReadId } = props;
+  // console.log('otherUser: ', otherUser)
+
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
-
+        const isLatestReadMessage = latestMessageReadId === message.id;
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} messageIsRead={message.read} otherUser={otherUser} isLatestReadMessage={isLatestReadMessage} />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} messageId={message.id} messageIsRead={message.read} conversationId={conversationId} />
         );
