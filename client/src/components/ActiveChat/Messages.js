c3853f2b3fd64eb3f6e88097428aabfaddad9897
jsmithb117 <jsmithb117@gmail.com>
2021-12-12 17:05:20 -0800

    Stores latestMessageId in state so child components will rerender when a new message has been read.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId, conversationId, latestMessageReadId } = props;
+  const [latestMessageId, setLatestMessageId] = useState(null);
+  useEffect(() => {
+    setLatestMessageId(latestMessageReadId);
+  }, [latestMessageReadId]);
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
-        const isLatestReadMessage = latestMessageReadId === message.id;
+        const isLatestReadMessage = latestMessageId === message.id;
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} messageIsRead={message.read} otherUser={otherUser} isLatestReadMessage={isLatestReadMessage} />
         ) : (
