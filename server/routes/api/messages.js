5217d2b117300e20ce081fa01db829d5b7fedce3
jsmithb117 <jsmithb117@gmail.com>
2021-12-14 08:37:07 -0800

    Adds default 'read' value to new Messages. Removes read property when creating a new Message.

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -13,9 +13,7 @@ router.post("/", async (req, res, next) => {
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const read = false;
-      const message = await Message.create({ senderId, text, conversationId, read });
-      const conversation = await Conversation.findOne({ where: { id: conversationId } });
+      const message = await Message.create({ senderId, text, conversationId });
 
       return res.json({ message, sender });
     }
@@ -39,7 +37,6 @@ router.post("/", async (req, res, next) => {
       senderId,
       text,
       conversationId: conversation.id,
-      read: false,
     });
     res.json({ message, sender });
   } catch (error) {
