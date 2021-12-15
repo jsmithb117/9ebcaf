b1998bad8f77a08e1bffc9abad432082b2e5bb24
jsmithb117 <jsmithb117@gmail.com>
2021-12-15 14:42:01 -0800

    Changes a component to self-close.

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -43,7 +43,7 @@ const SenderBubble = (props) => {
         <Typography className={classes.text}>{text}</Typography>
       </Box>
       {isLatestReadMessage &&
-        <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>
+        <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar} />
       }
 
     </Box>
