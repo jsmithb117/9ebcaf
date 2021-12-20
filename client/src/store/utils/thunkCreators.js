f2f8ca39bd825e4203fa7cd3255fb0cf10122720
jsmithb117 <jsmithb117@gmail.com>
2021-12-20 12:57:00 -0800

    Fixes error where data was not being awaited.

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -97,13 +97,11 @@ const sendMessage = (data, body) => {
 export const postMessage = (body) => async (dispatch) => {
   try {
     const data = await saveMessage(body);
-
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
       dispatch(setNewMessage(data.message));
     }
-
     sendMessage(data, body);
   } catch (error) {
     console.error(error);
